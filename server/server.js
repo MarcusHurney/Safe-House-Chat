d760dc8d5884c1125e72035a2c7e3618d5d97154
Marcus Hurney <marcushurney@gmail.com>
2016-10-16 22:23:45 -0400

    Sets up socket.io

--- server/server.js
+++ server/server.js
@@ -1,10 +1,24 @@
 const path = require('path');
+const http = require('http');
 const publicPath = path.join(__dirname, '../public');
+const socketIO = require('socket.io');
+
 const express = require('express');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+const server = http.createServer(app);
+const io = socketIO(server);
+
 app.use(express.static(publicPath));
-app.listen(PORT, () => {
+
+io.on('connection', socket => {
+  console.log("New user connected");
+  socket.on('disconnect', () => {
+    console.log("Client disconnected");
+  });
+});
+
+server.listen(PORT, () => {
   console.log("Server is available on port 3000");
 });
